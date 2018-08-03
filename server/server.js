34b601bdb354d95d31574b4f7f4a6eaa894c1034
Dorian Wojda <info@it-wojda.de>
2018-08-03 17:19:25 +0200

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -15,13 +15,33 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: "Admin",
+    text: "Welcome to Server",
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: "Admin",
+    text: "New user joined",
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (msg) => {
     console.log('Handle new message', msg);
-    io.emit('newMessage', {
+
+    // io.emit('newMessage', {
+    //   from: msg.from,
+    //   text: msg.text,
+    //   createdAt: new Date().getTime()
+    // });
+
+    socket.broadcast.emit('newMessage', {
       from: msg.from,
       text: msg.text,
       createdAt: new Date().getTime()
     });
+
   });
 
   socket.on('disconnect', (socket) => {
